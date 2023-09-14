57672e9e90f85b4c2290a4e58abeec6e40c90ce7
NomsuBen <olaniranbenjamin@.com>
2023-09-14 17:07:49 +0100

    Comments added

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
+/**
+    a script that computes the number of tasks completed by user id.
 
+The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+Only print users with completed task
+You must use the module request
+ */
 const request = require('request');
 const url = process.argv[2];
 
--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
+/**
+     a script that prints all characters of a Star Wars movie:
 
+The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+Display one character name by line
+You must use the Star wars API
+You must use the module request
+ */
 const request = require('request');
 
 const movieId = process.argv[2];
